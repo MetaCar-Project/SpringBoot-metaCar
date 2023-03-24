ca7a391f568cd812b420c2bd7d2fc151c249d696
insung <pp21356@naver.com>
2023-03-24 14:20:10 +0900

    3월24일 DTO Service Mapper 작성, Security 아주초기설정

--- src/main/java/com/example/metacar/mapper/ReturnMapper.java
+++ src/main/java/com/example/metacar/mapper/ReturnMapper.java
@@ -1,7 +1,18 @@
 package com.example.metacar.mapper;
 
+import com.example.metacar.dto.Rental_CarDTO;
+import com.example.metacar.dto.Return_CarDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class ReturnMapper {
+public interface ReturnMapper {
+    public void carReturn(Return_CarDTO rc);
+
+    public int returnUpdate(String carNum);
+
+    public int carReturnox(int reserveNum);
+
+    public String getCarnum(int reserveNum);
+
+    public Rental_CarDTO getRental(String id);
 }
