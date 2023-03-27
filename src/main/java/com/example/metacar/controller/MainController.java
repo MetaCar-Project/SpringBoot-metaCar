117072a2bc1adc20c25ed985c1a5893252c5ce1e
IAMWith <hoons1007@naver.com>
2023-03-27 18:47:56 +0900

    0327/이상훈 메인 작업/ 이상훈

--- src/main/java/com/example/metacar/controller/MainController.java
+++ src/main/java/com/example/metacar/controller/MainController.java
@@ -1,7 +1,41 @@
 package com.example.metacar.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.metacar.dto.Criteria;
+import com.example.metacar.dto.Have_CarDTO;
+import com.example.metacar.dto.PageDTO;
+import com.example.metacar.dto.Rental_CarDTO;
+import com.example.metacar.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8082")
 @RestController
-public class MainController {
+public class  MainController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/metaCar/main")
+    public List<Have_CarDTO> main(Criteria cri) {
+        return carService.carWithPaginggetList(cri);
+    }
+
+    @GetMapping("/metaCar/detail/{carNum}")
+    public Have_CarDTO detail(@PathVariable("carNum") String carNum){
+        return  carService.cardetail(carNum);
+    }
+
+    @PostMapping("/main")
+    @ResponseBody
+    public ResponseEntity<Rental_CarDTO> main(@RequestBody String id, HttpServletResponse response){
+        String checkid=id.trim().substring(1).substring(0, id.length()-2);
+        response.setHeader("Content-Type", "application/json;charset=utf-8");
+        return new ResponseEntity<Rental_CarDTO> (carService.carMain(checkid), HttpStatus.OK);
+    }
 }
